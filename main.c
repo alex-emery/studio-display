05e156465989020e57e983fe9740dd59f9a9a3d2
alex emery <alex.emery@couchbase.com>
2023-07-28 18:52:13 +0100

    auto selects monitor
    ihavenoideawhatimdoing
    
    and it shows.
    
    using udev to try enumerate through /dev/hidRawX to find
    the appropriate device before sending

--- main.c
+++ main.c
@@ -1,52 +1,134 @@
-#include <fcntl.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <libudev.h>
+#include <linux/hidraw.h>
+#include <linux/input.h>
+#include <linux/types.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#define APPLE_STUDIO_DISPLAY_VENDOR_ID "05ac"
+#define APPLE_STUDIO_DISPLAY_PRODUCT_ID "1114"
+
+char correct_report[4] = {0x05, 0x80, 0x09, 0x01};
+int checkReport(char *incoming) {
+  for (int i = 0; i < 4; i++) {
+    if (incoming[i] != correct_report[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
 int main(int argc, char **argv) {
-  int fd;
-  int res = 0;
-  char buf[7];
 
-  char *device = "/dev/hidraw0";
-
-  // default brightness, goes from 60000 to 4000
   long brightness = 60000;
-  if (argc > 1)
-    device = argv[1];
-
-  if (argc > 2)
-    brightness = strtol(argv[2], NULL, 10);
+  if (argc > 1) {
+    brightness = strtol(argv[1], NULL, 10);
+  }
 
   char brightnessLE[2];
   brightnessLE[0] = brightness & 0xff;
   brightnessLE[1] = brightness >> 8;
 
-  /* Open the Device with non-blocking reads. In real life,
-     don't use a hard coded path; use libudev instead. */
-  fd = open(device, O_RDWR | O_NONBLOCK);
+  struct udev *udev;
+  struct udev_enumerate *enumerate;
+  struct udev_list_entry *devices, *dev_list_entry;
+  struct udev_device *dev;
 
-  if (fd < 0) {
-    perror("Unable to open device");
-    return 1;
+  /* Create the udev object */
+  udev = udev_new();
+  if (!udev) {
+    printf("Can't create udev\n");
+    exit(1);
   }
 
-  memset(buf, 0x0, sizeof(buf));
+  enumerate = udev_enumerate_new(udev);
+  udev_enumerate_add_match_subsystem(enumerate, "hidraw");
+  udev_enumerate_scan_devices(enumerate);
+  devices = udev_enumerate_get_list_entry(enumerate);
 
-  buf[0] = 0x01;
-  buf[1] = brightnessLE[0];
-  buf[2] = brightnessLE[1];
+  udev_list_entry_foreach(dev_list_entry, devices) {
+    const char *path;
 
-  res = write(fd, buf, 7);
-  if (res < 0) {
-    printf("Error: %d\n", errno);
-    perror("write");
-  } else {
-    printf("write() wrote %d bytes\n", res);
+    path = udev_list_entry_get_name(dev_list_entry);
+    dev = udev_device_new_from_syspath(udev, path);
+
+    const char *devicePath = udev_device_get_devnode(dev);
+
+    dev =
+        udev_device_get_parent_with_subsystem_devtype(dev, "usb", "usb_device");
+    if (!dev) {
+      printf("Unable to find parent usb device.");
+      exit(1);
+    }
+
+    const char *idVendor = udev_device_get_sysattr_value(dev, "idVendor");
+    if (strcmp(idVendor, APPLE_STUDIO_DISPLAY_VENDOR_ID)) {
+      udev_device_unref(dev);
+      continue;
+    }
+
+    const char *idProduct = udev_device_get_sysattr_value(dev, "idProduct");
+    if (strcmp(idProduct, APPLE_STUDIO_DISPLAY_PRODUCT_ID)) {
+      udev_device_unref(dev);
+      continue;
+    }
+
+    udev_device_unref(dev);
+
+    int fd = open(devicePath, O_RDWR | O_NONBLOCK);
+
+    if (fd < 0) {
+      perror("Unable to open device");
+      return 1;
+    }
+
+    int i, res, desc_size = 0;
+    struct hidraw_report_descriptor rpt_desc;
+    struct hidraw_devinfo info;
+    char buf[7];
+    memset(buf, 0x0, sizeof(buf));
+
+    /* Get Report Descriptor Size */
+    res = ioctl(fd, HIDIOCGRDESCSIZE, &desc_size);
+    if (res < 0) {
+      close(fd);
+      perror("HIDIOCGRDESCSIZE");
+      continue;
+    }
+
+    rpt_desc.size = desc_size;
+    res = ioctl(fd, HIDIOCGRDESC, &rpt_desc);
+    if (res < 0) {
+      perror("HIDIOCGRDESC");
+      close(fd);
+      continue;
+    }
+
+    if (checkReport(rpt_desc.value) == 0) {
+      close(fd);
+      continue;
+    }
+
+    memset(buf, 0x0, sizeof(buf));
+    buf[0] = 0x1;
+    buf[1] = brightnessLE[0];
+    buf[2] = brightnessLE[1];
+    res = write(fd, buf, 7);
+    if (res < 0) {
+      printf("Error: %d\n", errno);
+    }
+    close(fd);
+    break;
   }
 
-  close(fd);
-  return 0;
+  /* Free the enumerator object */
+  udev_enumerate_unref(enumerate);
+
+  udev_unref(udev);
 }
