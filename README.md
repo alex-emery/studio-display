c78d4c01c7ad832dc666827951865eede64c09b4
alex emery <alex.emery@couchbase.com>
2023-07-29 09:49:16 +0100

    refactored
    moved everything out of main to try make it more readable

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 A very hacked together "program" that sends just the right 
 bytes to let you change a Studio Display's brightness.
 
+Will iterate through available `/dev/hidrawX` until it finds 
+a matching vendor id + product id with the correct report description.
+Once found it'll send the appropriate byte array to set the brightness.
 
 ## Usage
-
-`gcc ./main.c -o studio`
-`./studio /dev/hidraw3 5000`
+`gcc ./main.c -ludev -o studio `
+`./studio 50000`
 
 
--- main.c
+++ main.c
@@ -14,6 +14,9 @@
 #define APPLE_STUDIO_DISPLAY_PRODUCT_ID "1114"
 
 char correct_report[4] = {0x05, 0x80, 0x09, 0x01};
+// checks the report descriptor byte array
+// for vendor id, product id, and making
+// sure its a monitor page
 int checkReport(char *incoming) {
   for (int i = 0; i < 4; i++) {
     if (incoming[i] != correct_report[i]) {
@@ -24,16 +27,91 @@ int checkReport(char *incoming) {
   return 1;
 }
 
-int main(int argc, char **argv) {
+// attempts to open the device
+// and retrieve a report descriptor
+int checkDevice(const char *devicePath) {
 
-  long brightness = 60000;
-  if (argc > 1) {
-    brightness = strtol(argv[1], NULL, 10);
+  int fd = open(devicePath, O_RDWR | O_NONBLOCK);
+
+  if (fd < 0) {
+    perror("Unable to open device");
+    return 1;
   }
 
+  int res, desc_size = 0;
+  struct hidraw_report_descriptor rpt_desc;
+  struct hidraw_devinfo info;
+
+  /* Get Report Descriptor Size */
+  res = ioctl(fd, HIDIOCGRDESCSIZE, &desc_size);
+  if (res < 0) {
+    close(fd);
+    perror("HIDIOCGRDESCSIZE");
+    return 1;
+  }
+
+  rpt_desc.size = desc_size;
+  res = ioctl(fd, HIDIOCGRDESC, &rpt_desc);
+  if (res < 0) {
+    perror("HIDIOCGRDESC");
+    close(fd);
+    return 1;
+  }
+
+  if (checkReport(rpt_desc.value) == 0) {
+    close(fd);
+    return 1;
+  }
+
+  close(fd);
+  return 0;
+}
+
+// sends the brightness value to the specified device.
+// monitor expects little endian so we handle that
+// conversion here as well.
+int writeBrightness(const char *devicePath, int brightness) {
+
+  int fd = open(devicePath, O_RDWR | O_NONBLOCK);
+  if (fd < 0) {
+    perror("Unable to open device");
+    return 1;
+  }
+
+  int res = 0;
   char brightnessLE[2];
+
   brightnessLE[0] = brightness & 0xff;
   brightnessLE[1] = brightness >> 8;
+
+  char buf[7];
+  memset(buf, 0x0, sizeof(buf));
+
+  buf[0] = 0x1;
+  buf[1] = brightnessLE[0];
+  buf[2] = brightnessLE[1];
+  res = write(fd, buf, 7);
+  close(fd);
+  if (res < 0) {
+    printf("Error: %d\n", errno);
+    return 1;
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+
+  int brightness = 60000;
+  if (argc > 1) {
+    brightness = atoi(argv[1]);
+  }
+
+  if (brightness < 400 || brightness > 60000) {
+    printf("specified brightness %d must be between 400 and 60000\n",
+           brightness);
+    exit(1);
+  }
 
   struct udev *udev;
   struct udev_enumerate *enumerate;
@@ -79,51 +157,17 @@ int main(int argc, char **argv) {
       continue;
     }
 
+    if (checkDevice(devicePath) == 1) {
+      udev_device_unref(dev);
+      continue;
+    }
+
+    if (writeBrightness(devicePath, brightness)) {
+      printf("failed to set brightness for device %s", devicePath);
+    }
+
     udev_device_unref(dev);
 
-    int fd = open(devicePath, O_RDWR | O_NONBLOCK);
-
-    if (fd < 0) {
-      perror("Unable to open device");
-      return 1;
-    }
-
-    int i, res, desc_size = 0;
-    struct hidraw_report_descriptor rpt_desc;
-    struct hidraw_devinfo info;
-    char buf[7];
-    memset(buf, 0x0, sizeof(buf));
-
-    /* Get Report Descriptor Size */
-    res = ioctl(fd, HIDIOCGRDESCSIZE, &desc_size);
-    if (res < 0) {
-      close(fd);
-      perror("HIDIOCGRDESCSIZE");
-      continue;
-    }
-
-    rpt_desc.size = desc_size;
-    res = ioctl(fd, HIDIOCGRDESC, &rpt_desc);
-    if (res < 0) {
-      perror("HIDIOCGRDESC");
-      close(fd);
-      continue;
-    }
-
-    if (checkReport(rpt_desc.value) == 0) {
-      close(fd);
-      continue;
-    }
-
-    memset(buf, 0x0, sizeof(buf));
-    buf[0] = 0x1;
-    buf[1] = brightnessLE[0];
-    buf[2] = brightnessLE[1];
-    res = write(fd, buf, 7);
-    if (res < 0) {
-      printf("Error: %d\n", errno);
-    }
-    close(fd);
     break;
   }
 
